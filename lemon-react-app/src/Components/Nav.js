b6faf76e2f87acfae5422c7f9f7617f671a44d20
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2024-07-17 01:01:59 +0200

    refactoring

--- lemon-react-app/src/Components/Nav.js
+++ lemon-react-app/src/Components/Nav.js
@@ -1,22 +1,16 @@
 // Nav.js
+
 import React from 'react';
 import '../styles/nav.css';
-import logo from '../assets/Logo.svg'; // Adjust the path as per your file structure
+import logo from '../assets/Logo.svg';
 
 function Nav() {
-    const scrollToAboutUs = () => {
-        const aboutUsSection = document.getElementById('about-us-section');
-        if (aboutUsSection) {
-            aboutUsSection.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
-
     return (
         <nav className="navbar">
             <img src={logo} alt="Logo" className="nav-logo" />
             <ul className="nav-items">
                 <li><a href="#home">Home</a></li>
-                <li><a href="#about" onClick={scrollToAboutUs}>About</a></li>
+                <li><a href="#about" onClick={scrollToAbout}>About</a></li>
                 <li><a href="#services">Services</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
@@ -25,3 +19,11 @@ function Nav() {
 }
 
 export default Nav;
+
+function scrollToAbout(event) {
+    event.preventDefault();
+    const aboutUsSection = document.getElementById('about-us-section');
+    if (aboutUsSection) {
+        aboutUsSection.scrollIntoView({ behavior: 'smooth' });
+    }
+}
