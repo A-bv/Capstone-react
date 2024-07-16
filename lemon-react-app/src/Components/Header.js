741ab69fee2356ea1195fb1a76ff5fb60f524ab0
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2024-07-16 16:22:29 +0200

    Header Styling

--- lemon-react-app/src/Components/Header.js
+++ lemon-react-app/src/Components/Header.js
@@ -7,16 +7,17 @@ import restaurantFood from '../Assets/restaurantFood.jpg'; // Import the image
 function Header() {
     return (
         <header className="header">
-            <div className="header-text">
-                <div className="text-content">
+            <div className="header-left">
+                <div className="header-text">
                     <h1>Little Lemon</h1>
                     <h2>Chicago</h2>
-                    <br></br>
                     <p>Welcome to our delightful restaurant located in the heart of Chicago. Enjoy our fresh and delicious meals prepared with love and care.</p>
                 </div>
             </div>
-            <div className="header-image">
-                <img src={restaurantFood} alt="Banner" className="header-img" />
+            <div className="header-right">
+                <div className="header-image">
+                    <img src={restaurantFood} alt="Banner" className="header-img" />
+                </div>
             </div>
         </header>
     );
