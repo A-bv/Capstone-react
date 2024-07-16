77b0c879f040ca80bafb04674ee0af3303d71fa1
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2024-07-17 01:08:48 +0200

    Contact handling

--- lemon-react-app/src/Components/Nav.js
+++ lemon-react-app/src/Components/Nav.js
@@ -1,29 +1,33 @@
 // Nav.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/nav.css';
 import logo from '../assets/Logo.svg';
+import ContactModal from './ContactModal'; // Import ContactModal component
 
 function Nav() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleContactClick = () => {
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+    };
+
     return (
         <nav className="navbar">
             <img src={logo} alt="Logo" className="nav-logo" />
             <ul className="nav-items">
                 <li><a href="#home">Home</a></li>
-                <li><a href="#about" onClick={scrollToAbout}>About</a></li>
+                <li><a href="#about">About</a></li>
                 <li><a href="#services">Services</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="#contact" onClick={handleContactClick}>Contact</a></li>
             </ul>
+            {showModal && <ContactModal onClose={closeModal} />}
         </nav>
     );
 }
 
 export default Nav;
-
-function scrollToAbout(event) {
-    event.preventDefault();
-    const aboutUsSection = document.getElementById('about-us-section');
-    if (aboutUsSection) {
-        aboutUsSection.scrollIntoView({ behavior: 'smooth' });
-    }
-}
