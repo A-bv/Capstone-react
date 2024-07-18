fc5f77eeff03c81a740e6ee76e56f9d96ab88f6f
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2024-07-18 02:18:56 +0200

    refactoring navigation

--- lemon-react-app/src/App.js
+++ lemon-react-app/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React, { useRef } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
@@ -14,25 +14,28 @@ function App() {
     const aboutUsRef = useRef(null);
 
     return (
-        /*<Router>*/
-            <div className="App">
-                <Nav aboutUsRef={aboutUsRef} />
-                <Routes>
-                    <Route path="/" element={
-                        <>
-                            <Header />
-                            <Main />
-                            <Testimonials />
-                            <div ref={aboutUsRef}>
-                                <AboutUs />
-                            </div>
-                            <Footer />
-                        </>
-                    } />
-                    <Route path="/booking" element={<Booking />} />
-                </Routes>
+        <div className="App">
+            <Nav aboutUsRef={aboutUsRef} />
+            <Routes>
+                <Route path="/" element={<HomePage aboutUsRef={aboutUsRef} />} />
+                <Route path="/booking" element={<Booking />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
+}
+
+// Home page component for nested routing
+function HomePage({ aboutUsRef }) {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Testimonials />
+            <div ref={aboutUsRef}>
+                <AboutUs />
             </div>
-        /*</Router>*/
+        </>
     );
 }
 
