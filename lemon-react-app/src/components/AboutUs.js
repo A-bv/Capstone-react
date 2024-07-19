f2155c2b370a138b80fcf6754b63615aec7e1f0f
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2024-07-19 02:05:21 +0200

    images

--- lemon-react-app/src/components/AboutUs.js
+++ lemon-react-app/src/components/AboutUs.js
@@ -2,7 +2,8 @@
 
 import React from 'react';
 import '../styles/aboutus.css';
-import restaurantInterior from '../assets/restaurantInterior.jpg';
+import restaurantInterior from '../assets/teama.jpg';
+import secondImage from '../assets/teamb.jpg';
 
 function AboutUs() {
     return (
@@ -11,8 +12,13 @@ function AboutUs() {
                 <h2>About Us</h2>
                 <p>Welcome to Little Lemon, located in the heart of Chicago. Our restaurant offers a delightful array of meals prepared with love and care. Our story began many years ago, and we have been committed to providing our customers with the best dining experience since then.</p>
             </div>
-            <div className="about-us-image">
-                <img src={restaurantInterior} alt="Restaurant Interior" />
+            <div className="about-us-images">
+                <div className="about-us-image first-image">
+                    <img src={restaurantInterior} alt="Interior view of Little Lemon restaurant" />
+                </div>
+                <div className="about-us-image second-image">
+                    <img src={secondImage} alt="Another view of the restaurant" />
+                </div>
             </div>
         </section>
     );
