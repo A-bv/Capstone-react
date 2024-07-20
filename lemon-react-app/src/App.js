5bed438b2725b40a13ac8d18d5e1913f6a71688a
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2024-07-20 23:44:30 +0200

    deploy

--- lemon-react-app/src/App.js
+++ lemon-react-app/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React, { useRef } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
@@ -10,24 +10,27 @@ import Testimonials from './components/Testimonials';
 import AboutUs from './components/AboutUs';
 import Booking from './components/Booking';
 import restaurantFood from './assets/restaurantFood.jpg';
+import wallpaper from './assets/wallpaper.jpg';
 
 function App() {
     const aboutUsRef = useRef(null);
 
     return (
-        <div className="App">
-            <Nav aboutUsRef={aboutUsRef} />
-            <Routes>
-                <Route path="/" element={<HomePage aboutUsRef={aboutUsRef} />} />
-                <Route path="/booking" element={<BookingPage />} />
-            </Routes>
-            <Footer />
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Nav aboutUsRef={aboutUsRef} />
+                <Routes>
+                    <Route path="/" element={<HomePageScreen aboutUsRef={aboutUsRef} />} />
+                    <Route path="/booking" element={<BookingPage />} />
+                </Routes>
+                <Footer />
+            </div>
+        </BrowserRouter>
     );
 }
 
 // Home page component for nested routing
-function HomePage({ aboutUsRef }) {
+function HomePageScreen({ aboutUsRef }) {
     return (
         <>
             <Header
@@ -53,7 +56,7 @@ function BookingPage() {
                 title="Book a Table"
                 subtitle="Reserve your table at Little Lemon today!"
                 description="Reserve your table at Little Lemon today!"
-                imageSrc={restaurantFood}
+                imageSrc={wallpaper}
             />
             <Booking />
         </>
